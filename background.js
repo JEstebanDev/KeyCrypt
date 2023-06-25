44df79aea53410e407169f10b0a64b20afac7294
jestebancdev <jestebancdev@gmail.com>
2023-06-24 19:43:41 -0500

    :lipstick: adding the style and validating the errors

--- background.js
+++ background.js
@@ -3,12 +3,16 @@ function checkLocalStorage() {
   var value = localStorage.getItem("pattern");
   const formWelcome = document.getElementById("welcome");
   const formEncrypt = document.getElementById("KeyField");
+  const formResult = document.getElementById("Result");
   if (value == null) {
     formWelcome.style.display = "block";
     formEncrypt.style.display = "none";
+    formResult.style.display = "none";
   } else {
     formEncrypt.style.display = "block";
+    formResult.style.display = "none";
     formWelcome.style.display = "none";
+    document.getElementsByTagName("html")[0].style.height="100px";
   }
 }
 checkLocalStorage();
